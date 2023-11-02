9bd6aa00d6253212722cc5b730d1b7695d4078bb
Alexiotovv <gpsalexvasquez@gmail.com>
2023-11-02 07:54:23 -0500

    corregido_menu

--- resources/views/bases/base.blade.php
+++ resources/views/bases/base.blade.php
@@ -456,9 +456,9 @@
 {{-- <script src="../../../assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script> --}}
 
 
-{{-- <script src="../../../assets/js/index.js"></script> --}}
+<script src="../../../assets/js/index.js"></script>
 <!--app JS-->
-{{-- <script src="../../../assets/js/app.js"></script> --}}
+<script src="../../../assets/js/app.js"></script>
 {{-- <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
 integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA=="
 data-cf-beacon='{"rayId":"79b770648a486dfe","version":"2023.2.0","r":1,"token":"52ad871770524b3e8ca1e53094efe67a","si":100}'
