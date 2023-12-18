5815c3535d256935d0c5ff9f14acfda10e7511a7
Journii <ginopaflo001608@gmail.com>
2023-12-18 12:16:45 -0500

    fixed tabla

--- resources/views/livewire/alertas/panel-view.blade.php
+++ resources/views/livewire/alertas/panel-view.blade.php
@@ -1,22 +1,15 @@
 <div>
     <div class="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-right pl-2" style="width:80%;right:0;top:0px; paddin-left:10px; {{ $display }}" id="PanelInfoSoS">
         <div class="row">
-            <div class="col-md-8 ml-3 bg-warning">
-                <div class="logo-text ml-3" style="color:rgb(0, 0, 0);">DETALLES DE SOS</div>
-            </div>
-            <div class="col-md-4 p-0 bg-warning" style="border-radius: 0% !important">
-                <a  wire:click.prevent="close" class="btn btn-dark w-100">Cerrar</a>
-            </div>
-            <div class="col-md-12">
-                <iframe width="100%" height="290" 
-                    src="https://www.openstreetmap.org/export/embed.html?bbox={{ $longitud-0.002 }}%2C{{ $latitud-0.002 }}%2C{{ $longitud+0.002 }}%2C{{ $latitud+0.002 }}&amp;layer=mapnik&amp;marker={{ $latitud }}%2C{{ $longitud }}" 
-                    style="border: 1px solid black"></iframe>
+            <div class="col-md-12 p-0 bg-warning col-md-offset-1" style="border-radius: 0% !important">
+                <a  wire:click.prevent="close" class="w3-bar-item w3-button w3-hide-large card-title float-right" style="font-weight: 12"> <i class="lni lni-close"></i> Cerrar</a>
             </div>
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Datos de Usuario</h5>
-                        <table class="table mb-0">
+                        <hr>
+                        <table class="table mb-0 border">
                             <thead>
                                 <tr>
                                     <th scope="col">Nombre</th>
@@ -38,6 +31,11 @@
                         </table>
                     </div>
                 </div>
+            </div>
+            <div class="col-md-12">
+                <iframe width="100%" height="290" 
+                    src="https://www.openstreetmap.org/export/embed.html?bbox={{ $longitud-0.002 }}%2C{{ $latitud-0.002 }}%2C{{ $longitud+0.002 }}%2C{{ $latitud+0.002 }}&amp;layer=mapnik&amp;marker={{ $latitud }}%2C{{ $longitud }}" 
+                    style="border: 1px solid black"></iframe>
             </div>
         </div>
     </div>    
